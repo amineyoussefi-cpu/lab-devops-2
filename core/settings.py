0dc2ecbf2de74fa01c2ea336ecaea3947c0ccff6
amineyoussefi-cpu <amine.youssefi.27@eigsica.ma>
2026-05-04 11:09:16 +0100

    Update ALLOWED_HOSTS to include Vercel URL

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yy@zb(agg_5ff51muwau@0usv+hmv)&)yzdyho2w#3o32!(j3(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'melodious-trust-production.up.railway.app']
 
 
 # Application definition
